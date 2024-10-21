b51279f4d5fbf359a6797ef653dd1c88c402318b
Pedro Rodrigues <pedrohrodrigues86@gmail.com>
2024-10-20 21:24:22 -0300

    exercicos finais
     Exercicio de fibonacci finalizado e exercicios em texto

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,43 +1,23 @@
-let number = 8; // Inserir número para verificar se ele pertence à sequência de Fibonacci
-
-let quadradoPerfeito = false;
-let binetPositivo;
-let binetNegativo;
+let number = 1597; // Inserir número para verificar se ele pertence à sequência de Fibonacci
 let a = 0;
 let b = 1;
+let c = 0;
 
-// Função para aplicar a equação de Binet
-function equacaoBinet(number) {
-  binetPositivo = 5 * number ** 2 + 4;
-  binetNegativo = 5 * number ** 2 - 4;
-}
-
-// Função para verificar se um número é quadrado perfeito
-function calculoQuadradoPerfeito(binetPositivo, binetNegativo) {
-  // Verifica se a raiz quadrada é um inteiro
-  if (
-    Number.isInteger(Math.sqrt(binetPositivo)) == true ||
-    Number.isInteger(Math.sqrt(binetNegativo)) == true
-  ) {
-    return true;
-  } else {
-    return false;
-  }
+// faz o cálculo da sequência de fibonacci
+while (number > c) {
+  c = a + b;
+  a = b;
+  b = c;
 }
 
 // Validação do número fornecido
 if (number < 0) {
-  console.log("Números negativos não pertencem à sequência de Fibonacci");
+  console.log(`Números negativos não pertencem à sequência de Fibonacci`);
 } else if (number == 0 || number == 1) {
-  console.log("O número pertence à sequência de Fibonacci");
-} else if (true) {
-  console.log("O número pertence à sequência de Fibonacci");
-  for (let i = 0; i <= ; ++i) {
-    console.log(a);
-    let temp = a + b;
-    a = b;
-    b = temp;
-  }
+  console.log(`O número pertence à sequência de Fibonacci`);
+} else if (number == c) {
+  console.log(`O número pertence à sequência de Fibonacci`);
+  console.log(`O próximo número da sequência é ${a + number}`);
 } else {
-  console.log("O número não pertence à sequência de Fibonacci");
+  console.log(`O número não pertence à sequência de Fibonacci`);
 }
